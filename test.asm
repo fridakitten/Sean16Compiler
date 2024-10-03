11fbdf72e37a884eddd9eb0c899a0048ba2020ab
FridaDEV <157828245+fridakitten@users.noreply.github.com>
2024-10-03 17:38:15 +0200

    some stuff

--- test.asm
+++ test.asm
@@ -5,10 +5,10 @@ BTN:
 	STO R0 5
 	STO R1 15
 	STO R2 30
-	GDL R0 R0 R0 R1 5       ; R0 R1 => R0 R2 LEADING
-	GDL R0 R0 R2 R0 5       ; R0 R1 => R0 R3 TOP
-	GDL R2 R0 R2 R1 5       ; R3 R1 => R3 R2 TRAILING
-	GDL R0 R1 R2 R1 5       ; R0 R2 => R3 R2 BOTTOM
+	GDL R0 R0 R0 R1 5              ; R0 R1 => R0 R2 LEADING
+	GDL R0 R0 R2 R0 5              ; R0 R1 => R0 R3 TOP
+	GDL R2 R0 R2 R1 5              ; R3 R1 => R3 R2 TRAILING
+	GDL R0 R1 R2 R1 5              ; R0 R2 => R3 R2 BOTTOM
 	STO R0 7
 	STO R1 8
 	GDC R1 R0 69 7
@@ -19,16 +19,14 @@ BTN:
 	ADD R1 6
 	GDC R1 R0 116 7
 	JMP CURSOR
-CURSOR_COLOR:
-	IFQ 3 R12 2 2
-	RAN R20 1 155
-	IFQ 3 R12 1 3
-	GCS
-	JMP BTN
-	JMP CURSOR
 CURSOR:
-	MUS R10 R11 R12        ; Get Mouse Position
-	GPX R10 R11 R20        ; Drawing Cursor
-	JMP CURSOR_COLOR
+    MUS 0 0 R32
+	IFQ 3 R32 2 3                   ; Left click
+	MUS R10 R11 0                   ; Get Mouse Position
+	GDL R30 R31 R10 R11 R12         ; Drawing Cursor
+	MUS R30 R31 0                   ; Get Initial Mouse Position
+	IFQ 3 R32 1 2                   ; Right click
+	RAN R12 1 155
+	JMP CURSOR
 MAIN:
 	JMP BTN
